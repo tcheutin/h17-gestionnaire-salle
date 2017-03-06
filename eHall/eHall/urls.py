090ad8361c3f1e49fc0582ab685169b211c57448
GavX <alain.zakkour112@gmail.com>
2017-03-06 16:14:34 -0500

    fix urls

--- eHall/eHall/urls.py
+++ eHall/eHall/urls.py
@@ -18,10 +18,7 @@ from django.contrib import admin
 from accounts.views import (login_view, register_view, logout_view)
 
 urlpatterns = [
-    url(r'^event/', include('event.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^login/', login_view, name='login'),
-    url(r'^register/', register_view, name='register'),
-    url(r'^logout/', logout_view, name='logout'),
-    url(r'^', login_view, name='login'),
+    url(r'^', include('accounts.urls')),
+    url(r'^', include('event.urls')),
 ]
