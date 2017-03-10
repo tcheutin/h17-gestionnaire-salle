6a9d435d381bc4d000239af44d08982daa4584a2
Raphael Zumer <rzumer@gmail.com>
2017-03-10 17:02:02 -0500

    Change URL routes for consistent mapping

--- eHall/auditorium/urls.py
+++ eHall/auditorium/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^auditorium$', views.auditorium, name='auditorium'),
+    url(r'^$', views.auditorium, name='auditorium'),
 ]
--- eHall/event/urls.py
+++ eHall/event/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^event$', views.dashboard, name='dashboard'),
+    url(r'^$', views.dashboard, name='dashboard'),
 ]
