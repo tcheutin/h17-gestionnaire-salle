92879275fb2419cd7f262c0af3bd14533a0111ed
Raphael Zumer <rzumer@gmail.com>
2017-03-11 12:06:06 -0500

    Create AJAX requests and routes to event management forms

--- eHall/event/forms.py
+++ eHall/event/forms.py
@@ -1,8 +1,40 @@
 from django import forms
-from eHall.event.models import Event
-from eHall.auditorium.models import Auditorium
+from crispy_forms.helper import FormHelper
+from .models import Event
+from auditorium.models import Auditorium
 
-class EventForm(forms.ModelForm):
+class AddForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(AddForm, self).__init__(*args, **kwargs)
+
+        # If you pass FormHelper constructor a form instance
+        # It builds a default layout with all its fields
+        self.helper = FormHelper(self)
+
+    class Meta:
+        model = Event
+        fields = ('name', 'artist', 'image', 'startDate', 'endDate', 'description', 'auditorium',)
+        
+class EditForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(EditForm, self).__init__(*args, **kwargs)
+
+        # If you pass FormHelper constructor a form instance
+        # It builds a default layout with all its fields
+        self.helper = FormHelper(self)
+
+    class Meta:
+        model = Event
+        fields = ('name', 'artist', 'image', 'startDate', 'endDate', 'description', 'auditorium',)
+        
+class DeleteForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(DeleteForm, self).__init__(*args, **kwargs)
+
+        # If you pass FormHelper constructor a form instance
+        # It builds a default layout with all its fields
+        self.helper = FormHelper(self)
+
     class Meta:
         model = Event
         fields = ('name', 'artist', 'image', 'startDate', 'endDate', 'description', 'auditorium',)
