e0b94171e6c6cec54525d0d67af5bcfc33dc6f05
Raphael Zumer <rzumer@gmail.com>
2017-03-10 19:22:47 -0500

    Split event app into multiple templates

--- eHall/event/views.py
+++ eHall/event/views.py
@@ -1,6 +1,37 @@
 # Create your views here.
 
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Event
+
+# Set the active attribute to activate the appropriate navbar button
+eventContext = {
+    'active': 'event',
+}
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    events = Event.objects.all()
+    paginator = Paginator(events, 10) # Show 10 events per page
+    
+    page = request.GET.get('page')
+    try:
+        # Display the requested page
+        events = paginator.page(page)
+    except PageNotAnInteger:
+        # Display the first page if no valid page argument is provided
+        events = paginator.page(1)
+    except EmptyPage:
+        # Display the last page if the requested range exceeds the number of entries
+        events = paginator.page(paginator.num_pages)
+        
+    context = {
+        'events': events,
+    }  
+    return render(request, 'dashboard.html', {**eventContext, **context})
+    
+def edit(request, eventId):
+    event = Event.objects.get(pk=eventId)
+    context = {
+        'event': event,
+    }
+    return render(request, 'edit.html', {**eventContext, **context})
