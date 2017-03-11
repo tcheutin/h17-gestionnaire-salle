907e1b8ee1ec2b1fe4e0f2fcc017c067c12b4128
Raphael Zumer <rzumer@gmail.com>
2017-03-11 17:21:00 -0500

    Create filters for displaying numbers and currency

---
+++ eHall/eHall/templatetags/filters.py
@@ -0,0 +1,13 @@
+from django import template
+import locale
+locale.setlocale(locale.LC_ALL, 'en-CA')
+
+register = template.Library()
+
+@register.filter(name='currency')
+def formatCurrency(value):
+    return locale.currency(value, grouping=True)
+
+@register.filter(name='bigint')
+def formatInteger(value):
+    return locale.format('%d', value, grouping=True)
