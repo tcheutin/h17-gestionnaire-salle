7c68a209f07bb1a62533e84df95d2c587109e230
Raphael Zumer <rzumer@gmail.com>
2017-03-11 17:23:21 -0500

    Set debug to true locally and false in production

--- eHall/eHall/settings/production.py
+++ eHall/eHall/settings/production.py
@@ -1,5 +1,8 @@
 from eHall.settings.base import *
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
 # dj_database_url configuration for Heroku
 import dj_database_url
 DATABASES = {
