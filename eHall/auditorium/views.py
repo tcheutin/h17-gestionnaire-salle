97712f78580790aed2ef685c0843a0ddfaee2acf
GavX <alain.zakkour112@gmail.com>
2017-03-12 16:43:12 -0400

    affichage event/auditorium selon user

--- eHall/auditorium/views.py
+++ eHall/auditorium/views.py
@@ -12,19 +12,8 @@ auditoriumContext = {
 
 def auditorium(request):
     if request.user.is_authenticated():
-        auditoriums = Auditorium.objects.all()
-        paginator = Paginator(auditoriums, 10) # Show 10 auditoriums per page
-
         page = request.GET.get('page')
-        try:
-            # Display the requested page
-            auditoriums = paginator.page(page)
-        except PageNotAnInteger:
-            # Display the first page if no valid page argument is provided
-            auditoriums = paginator.page(1)
-        except EmptyPage:
-            # Display the last page if the requested range exceeds the number of entries
-            auditoriums = paginator.page(paginator.num_pages)
+        auditoriums = getAuditoriumPage(request, page)
 
         context = {
             'auditoriums': auditoriums,
@@ -107,10 +96,11 @@ def getDeleteForm(request, auditoriumId):
     }
     return render(request, 'deleteForm.html', {**auditoriumContext, **context})
     
-def getAuditoriumPage(page):
-    auditoriums = Auditorium.objects.order_by('-id')
+def getAuditoriumPage(request, page):
+    user = request.user
+    auditoriums = Auditorium.objects.all().order_by('-id') if request.user.is_superuser else Auditorium.objects.filter(creator=user.id).order_by('-id')
     paginator = Paginator(auditoriums, 10) # Show 10 auditoriums per page
-    
+
     try:
         # Display the requested page
         auditoriums = paginator.page(page)
